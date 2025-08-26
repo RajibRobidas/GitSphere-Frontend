13123f684d4b4f3e9c1f1c85877a3e503199bb89
Rajib <rajibrobidas22222@gmail.com>
2025-08-26 10:27:19 +0600

    third commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
 import { AuthProvider } from './authContext.jsx'
 import { BrowserRouter, Router } from 'react-router-dom';
 import ProjectRoutes from './Routes.jsx'
